18113483987abe608f6a63aa59bed51a1bdd6da9
Marshall Bowers <elliott.codes@gmail.com>
2014-11-24 12:07:46 -0500

    Add more simple tests

---
+++ test/spec/note/expect.js
@@ -0,0 +1,14 @@
+(function () {
+    var hotel_name = 'hotel';
+    var hotel_note = 'A grand hotel.';
+
+    return {
+        feedback: '',
+        name: hotel_name,
+        note: hotel_note,
+        program: '',
+        usage: '',
+        children: [
+        ]
+    };
+})();
---
+++ test/spec/usage/expect.js
@@ -0,0 +1,14 @@
+(function () {
+    var hotel_name = 'hotel';
+    var hotel_usage = 'check into';
+
+    return {
+        feedback: '',
+        name: hotel_name,
+        note: '',
+        program: '',
+        usage: hotel_usage,
+        children: [
+        ]
+    };
+})();
