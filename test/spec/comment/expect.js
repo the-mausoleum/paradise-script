e8aeb6ec0b4f8639353446045ffe75692538f3f6
Marshall Bowers <elliott.codes@gmail.com>
2014-11-25 09:30:38 -0500

    Add support for paint command
    Include hex color code to RGB color conversion.

---
+++ test/spec/comment/expect.js
@@ -0,0 +1,14 @@
+(function () {
+    var hotel_name = 'hotel';
+    var hotel_note = 'This note should appear.';
+
+    return {
+        feedback: '',
+        name: hotel_name,
+        note: hotel_note,
+        program: '',
+        usage: '',
+        children: [
+        ]
+    };
+})();
