9817413a0d9589e233f4f3243fc5be638dcfb849
Marshall Bowers <elliott.codes@gmail.com>
2014-11-25 10:40:46 -0500

    Add separate tests for hex and RGB colors

---
+++ test/spec/paint/rgb/expect.js
@@ -0,0 +1,14 @@
+(function () {
+    var hotel_name = 'hotel';
+    var hotel_paint = '131 75 87';
+
+    return {
+        feedback: '',
+        name: hotel_name,
+        note: '',
+        program: '',
+        usage: '',
+        children: [
+        ]
+    };
+})();
